909019190bff159ef7b20ae45d9506903168ec36
CataVasquez <catavasquez204@gmail.com>
2022-10-19 16:45:46 -0500

    Se añade las cosas

---
+++ lib/feature/register/presentation/register_screen.dart
@@ -0,0 +1,132 @@
+import 'package:auth_ui/core/component/app_text_field.dart';
+import 'package:auth_ui/core/route/app_route_name.dart';
+import 'package:auth_ui/core/theme/app_color.dart';
+import 'package:auth_ui/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: AppColor.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+          bottom: 80,
+          left: 24,
+          right: 24,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              fit: BoxFit.contain,
+              image: const AssetImage("assets/img_register.png"),
+            ),
+            Text(
+              "Registro",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                AppTextField(
+                  prefix: Icon(Icons.person_outline),
+                  hint: "Usuario",
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(height: 24),
+                AppTextField(
+                  prefix: Icon(Icons.alternate_email_rounded),
+                  hint: "Correo electrónico",
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(height: 24),
+                AppTextField(
+                  prefix: Icon(Icons.phone_outlined),
+                  hint: "Celular",
+                  textInputAction: TextInputAction.done,
+                ),
+                
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Al registrarte, estás de acuerdo con nuestros ",
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                      TextSpan(
+                        text: "Términos y Condiciones",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: AppColor.primaryColor),
+                      ),
+                      TextSpan(
+                        text: " y ",
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                      TextSpan(
+                        text: "Políticas de privacidad",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: AppColor.primaryColor),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 64,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    ),
+                    child: const Text("Register"),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,143 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+import 'dart:ui';
+
+import 'package:auth_ui/util/my_card.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+import '../core/theme/app_color.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: AppColor.black,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      //barra de navegación inferior
+      backgroundColor: Colors.white,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.deepOrange.shade200,
+        animationDuration: Duration(milliseconds: 300),
+        onTap: (index){
+          print(index);
+        },
+        items: [
+          Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.favorite,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+        ]
+      ), 
+
+
+      body: Column(children: [
+        //app bar
+        
+        Padding(
+          
+          
+          padding: const EdgeInsets.symmetric(horizontal:25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SizedBox(height: 80),
+                  Text(
+                    'Mis',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    ' Tarjetas',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ],
+                ),
+
+                // plus button
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    shape: BoxShape.circle, 
+                    ),
+                    child: Icon(Icons.add),
+                ),
+            ],
+            ),
+
+          ),
+
+          SizedBox(height: 5),
+
+        //cards
+        Container(
+          height: 200,
+          child: PageView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              MyCard(
+                balance: 67.299,
+                cardNumber: 126985986, 
+                expiryMonth: 11, 
+                expiryYear: 10, 
+                color: Colors.deepOrange[300],
+              ),
+              MyCard(
+                balance: 990.999,
+                cardNumber: 10359867, 
+                expiryMonth: 10, 
+                expiryYear: 08, 
+                color: Colors.deepPurple[300],
+              ),
+              MyCard(
+                balance: 625.023,
+                cardNumber: 10025986, 
+                expiryMonth: 08, 
+                expiryYear: 15, 
+                color: Colors.blue[200],
+              ),
+            ],
+          ),
+        )
+
+        //buttons -> send + pay + bills
+
+        //column -> estadisticas + transacciones
+
+      ],)
+
+    );
+  }
+}
